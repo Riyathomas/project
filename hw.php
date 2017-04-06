94f18d27c9ea4d25d33ffdc7879dbf84498b0e32
Riyathomas <riyathomas218@gmail.com>
2017-04-06 11:33:31 +0530

    Add files via upload

---
+++ hw.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1></h1>
+
+<?php
+echo "“Hello, welcome to “baabtra”. Baabtra is a division of baabte. It’s one of the leading IT Training institute in Kerala.Baabtra has branches at “Calicut/Mukkam”,”Cochin/startup Village” etc”"
+?>
+
+</body>
+</html> 
